cc76cc01d508aed50074298b6afa0b84249d546d
DanielOjehon <g00466771@atu.ie>
2026-04-23 19:44:53 +0000

    fixing home page errors

--- src/app/pages/details/details.page.ts
+++ src/app/pages/details/details.page.ts
@@ -1,20 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.page.html',
-  styleUrls: ['./details.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule] 
 })
-export class DetailsPage implements OnInit {
+export class DetailsPage {
 
-  constructor() { }
+  movie: any;
 
-  ngOnInit() {
+  constructor(private router: Router) {
+    const nav = this.router.getCurrentNavigation();
+    this.movie = nav?.extras.state?.['movie'];
   }
-
 }
--- src/app/pages/favourites/favourites.page.ts
+++ src/app/pages/favourites/favourites.page.ts
@@ -1,20 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-favourites',
   templateUrl: './favourites.page.html',
-  styleUrls: ['./favourites.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
-export class FavouritesPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
+export class FavouritesPage {}
